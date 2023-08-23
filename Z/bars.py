d3457d8e6549849abdef26777fc97d36bc32b8f0
EDMSaden <sadenxd@gmail.com>
2023-08-23 09:03:40 +0500

    reName

--- Z/bars.py
+++ Z/bars.py
@@ -5,72 +5,9 @@ from PIL import ImageGrab
 import win32api, win32con
 import pyperclip
 
-def leftClick(*cord):
-    if len(cord) == 1:
-        cord = cord[0] 
-    x,y = cord 
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    time.sleep(0.003)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-
-def rightClick(*cord):
-    if len(cord) == 1:
-        cord = cord[0] 
-    x,y = cord 
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
-    time.sleep(0.003)
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
-
-
-def doubleClick(*cord):
-    if len(cord) == 1:
-        cord = cord[0] 
-    x,y = cord
-    for i in range(2):
-        win32api.SetCursorPos((x,y))
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-        time.sleep(0.003)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-
-
-keys = {'CTRL':0x11, 'ENTER':0x0D, 'PAGEUP':0x21,
-        'PAGEDOWN':0x22, 'ESC':0x1B , 'BACKSPACE':0x08, 'SPACE':0x20, 'SHIFT':0x10}
-
-def find_key(key : str):
-    key = key.upper()
-    if key in keys:
-        key = keys.get(key)
-    else: key = ord(key)
-    return key
-
-def press_key(key : str):
-
-    key = find_key(key)
-    win32api.keybd_event(key, 0,0,0)
-    time.sleep(.05)   
-    win32api.keybd_event(key,0 ,win32con.KEYEVENTF_KEYUP ,0)
-
-def hot_key(key_1 : str, key_2 : str):
-    key_1 = find_key(key_1)
-    key_2 = find_key(key_2)
-    win32api.keybd_event(key_1, 0,0,0)
-    time.sleep(.05)
-    win32api.keybd_event(key_2, 0,0,0)
-    time.sleep(.05)
-
-    win32api.keybd_event(key_1,0 ,win32con.KEYEVENTF_KEYUP ,0)
-    win32api.keybd_event(key_2,0 ,win32con.KEYEVENTF_KEYUP ,0)
-
-
-def write_text(text : str):
-    pyperclip.copy(text)
-    hot_key('ctrl', 'v')
-
 test = r'test'
 
-def lf_template(temp):
+def img_on_the_screen(temp):
     temp = f'{temp}.bmp'
     img_rgb = ImageGrab.grab()
     img_rgb = np.array(img_rgb)
@@ -116,16 +53,82 @@ def wait_bars(temp):
 
         else: break
 
-def change_row(cord,text):
-    leftClick(cord)
+def leftClickOn(*cord):
+    if type(cord[0]) is str:
+        cord = img_on_the_screen(cord[0])
+    elif len(cord) == 1:
+        cord = cord[0] 
+    x,y = cord 
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    time.sleep(0.003)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    wait_bars('wait_1')
+
+def rightClickOn(*cord):
+    if len(cord) == 1:
+        cord = cord[0] 
+    x,y = cord 
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
+    time.sleep(0.003)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
+    wait_bars('wait_1')
+
+
+def doubleClickOn(*cord):
+    if len(cord) == 1:
+        cord = cord[0] 
+    x,y = cord
+    for i in range(2):
+        win32api.SetCursorPos((x,y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+        time.sleep(0.003)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    wait_bars('wait_1')
+
+
+keys = {'CTRL':0x11, 'ENTER':0x0D, 'PAGEUP':0x21,
+        'PAGEDOWN':0x22, 'ESC':0x1B , 'BACKSPACE':0x08, 'SPACE':0x20, 'SHIFT':0x10}
+
+def find_key(key : str):
+    key = key.upper()
+    if key in keys:
+        key = keys.get(key)
+    else: key = ord(key)
+    return key
+
+def press_key(key : str):
+    key = find_key(key)
+    win32api.keybd_event(key, 0,0,0)
+    time.sleep(.05)   
+    win32api.keybd_event(key,0 ,win32con.KEYEVENTF_KEYUP ,0)
+    wait_bars('wait_1')
+
+def hot_key(key_1 : str, key_2 : str):
+    key_1 = find_key(key_1)
+    key_2 = find_key(key_2)
+    win32api.keybd_event(key_1, 0,0,0)
+    time.sleep(.05)
+    win32api.keybd_event(key_2, 0,0,0)
+    time.sleep(.05)
+
+    win32api.keybd_event(key_1,0 ,win32con.KEYEVENTF_KEYUP ,0)
+    win32api.keybd_event(key_2,0 ,win32con.KEYEVENTF_KEYUP ,0)
+    wait_bars('wait_1')
+
+
+def write_text(text : str):
+    pyperclip.copy(text)
+    hot_key('ctrl', 'v')
+
+def change_row(x,y,text):
+    leftClickOn(x,y)
     hot_key('ctrl','a')
     write_text(f'{text}')
-    time.sleep(1)
-    wait_bars('wait_1')
     press_key('enter')
-    wait_bars('wait_1')
 
     
 if __name__ == '__main__':
     while True:
-        lf_template(test)
+        img_on_the_screen(test)
--- Z/main.py
+++ Z/main.py
@@ -1,5 +1,5 @@
 
-from bars import lf_template,change_row,wait_bars, write_text, press_key, hot_key, leftClick, rightClick, doubleClick
+from bars import img_on_the_screen,change_row, write_text, press_key, hot_key, leftClickOn, rightClickOn, doubleClickOn
 from threading import Thread
 from time import sleep
 
@@ -10,7 +10,7 @@ sleep(3)
 def fix_error():
     count = 0
     while True:
-        if lf_template('error'):
+        if img_on_the_screen('error'):
             press_key('enter')
             count += 1
             print(f'Недействующий характер заболевания: {count}')
@@ -20,32 +20,23 @@ th.start()
 
 while True:
     for i in range(12):
-        if lf_template('start_1') and lf_template('start_2'):
-            leftClick(55,446 + distance)
+        if img_on_the_screen('start_1') and img_on_the_screen('start_2'):
+            leftClickOn(55,446 + distance)
             distance += 30
 
-            wait_bars('wait_1')
-
-            if lf_template('pos'):
-                change_row((986,245),2)
-                wait_bars('wait_1')
-                change_row((974,270),2)
-                wait_bars('wait_1')
-                change_row((702,470),6)
-                wait_bars('wait_1')
-                change_row((692,494),1)
-                wait_bars('wait_1')
-                if lf_template('result_1') and lf_template('result_2'):
+            if img_on_the_screen('pos'):
+                change_row(986,245,'2')   
+                change_row(974,270,'2')
+                change_row(702,470,'6')
+                change_row(692,494,'1')
+                
+                if img_on_the_screen('result_1') and img_on_the_screen('result_2'):
                     press_key('pagedown')
-                    wait_bars('wait_1')
-                    leftClick(lf_template('save'))
-                    wait_bars('wait_1')
-                    wait_bars('wait_1')
+                    leftClickOn('save')
+                    leftClickOn('save')
 
         press_key('esc')
-        wait_bars('wait_1')               
-    
-    if lf_template('ok'):
-        leftClick(lf_template('ok'))
-        distance = 0
-        wait_bars('wait_1')  
+
+    if img_on_the_screen('ok'):
+        leftClickOn(img_on_the_screen('ok'))
+        distance = 0         
